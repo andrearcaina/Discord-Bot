8bdad8feef876ac627f552ff435fb8584ef20373
Andre <dtandre331@gmail.com>
2023-02-10 10:48:03 -0500

    gitignore error

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     econ_bot = commands.Bot(command_prefix="!",help_command=None,intents=discord.Intents.all())
     #econ_bot = commands.Bot(command_prefix="!",intents=discord.Intents.all())
     
-    bot.run_bot(TOKEN,econ_bot)
+    bot.run_bot(TOKEN,econ_bot) #run bot
 
     #this is just a useless file tbh
     #but it makes it 'clean'
